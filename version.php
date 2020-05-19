9fbf02951907d952e6f41fa1f2cfa59218322c9e
trideepdm <trideep@dualcube.com>
2020-05-19 09:43:26 +0530

    version match

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_ddmatch';
-$plugin->version   = 2020041700;
+$plugin->version   = 2020041701;
 
 $plugin->requires  = 2013050100;
 $plugin->dependencies = array(
