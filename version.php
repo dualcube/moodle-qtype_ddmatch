6c6bbd5952e879887a8eca9af732705e2f311cbe
kundandualcube <kundan@dualcube.com>
2023-04-25 14:31:00 +0530

    version update 2.5

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_ddmatch';
-$plugin->version   = 2023031700;
+$plugin->version   = 2023042500;
 $plugin->requires  = 2012120301;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array(
     'qtype_match' => 2015111600,
 );
-$plugin->release = '2.4 (Build: 2023031700)';
+$plugin->release = '2.5 (Build: 2023042500)';
