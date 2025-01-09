0be407a070eb3c47e61eb6be2984275764c15d38
Purnendu Dash <32759662+purnendudash@users.noreply.github.com>
2025-01-09 11:02:35 +0530

    Update version.php

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_ddmatch';
-$plugin->version = 2025010800;
+$plugin->version = 2025010801;
 $plugin->requires = 2022112815;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'qtype_match' => 2015111600,
 );
-$plugin->release = '2.5.3 (Build: 2025010800)';
+$plugin->release = '2.5.3 (Build: 2025010801)';
